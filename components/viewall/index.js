a38776df4fda149fbc13e139639e0c4836b55fee
Matthew Pierce <lesbianhaircut@outlook.com>
2022-03-20 00:06:30 -0400

    c

--- components/viewall/index.js
+++ components/viewall/index.js
@@ -1,4 +1,15 @@
 import React from "react";
-import ViewAll from "./ContentHolder";
+import styled from "styled-components";
+import Sidebar from "./Sidebar";
+import COLORS from "../../Data/colors";
+import ArticleHolder from "./ArticleHolder";
+const Content = styled.div``;
 
-export default ViewAll;
+export default function ContentHolder(props) {
+  return (
+    <Content>
+      <Sidebar articles={props.articles} colors={COLORS}></Sidebar>
+      <ArticleHolder articles={props.articles} colors={COLORS} />
+    </Content>
+  );
+}
